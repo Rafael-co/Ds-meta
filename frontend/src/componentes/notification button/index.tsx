95dabad2b7482f3394df1ed8f558caf7c9dd052c
Rafael-co <rafael.camilo21@outlook.com>
2022-07-18 15:43:39 -0300

    Sale notification

--- frontend/src/componentes/notification button/index.tsx
+++ frontend/src/componentes/notification button/index.tsx
@@ -1,9 +1,20 @@
+import axios from 'axios';
 import icone from '../../assets/img/notification-icon.svg'
+import { BASE_URL } from '../../utils/request';
 import './styles.css'
-function NotificationButton() {
+type Props = {
+    saleID: number;
+}
+function handleClick(id :number) {
+    axios(`${BASE_URL}/sales/${id}/notification`)
+    .then (Response => {
+
+    });
+}
+function NotificationButton({saleID}: Props) {
     return (
 
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={() => {handleClick(saleID)}}>
             <img src={icone} alt="notificar" />
         </div>
 
