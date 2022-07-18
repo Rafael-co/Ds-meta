1023c970a2676118a91faf4e01be2bb165e67317
Rafael-co <rafael.camilo21@outlook.com>
2022-07-18 15:48:30 -0300

    toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from "./componentes/header"
 import SalesCard from "./componentes/salescard"
 function App() {
   return (
     <>
+    <ToastContainer />
       <h1><Header /></h1>
       <main>
         <section id="sales">
